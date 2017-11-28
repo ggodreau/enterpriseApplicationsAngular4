578be26cea88329786c13e513ddffb1234f11e5f
ggodreau <godreau@gmail.com>
2017-11-28 00:52:41 +0000

    added cordova check to platform.is() to turn off splash :rainbow::rage2:

--- section6/src/app/app.component.ts
+++ section6/src/app/app.component.ts
@@ -12,6 +12,8 @@ export class MyApp {
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
+      if (!platform.is('cordova'))
+        return;
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
