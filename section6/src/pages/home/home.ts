3f6d7796820f79305c419890680554c0dde04bc5
ggodreau <godreau@gmail.com>
2017-11-28 02:16:17 +0000

    added app version plugin to config.xml

--- section6/src/pages/home/home.ts
+++ section6/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+import { AppVersion } from 'ionic-native';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,12 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(
+    public navCtrl: NavController,
+    platform: Platform) {
+      platform.ready().then(() => {
+        AppVersion.getVersionNumber().then(v => console.log('app version is:', v));
+      }); 
   }
 
 }
