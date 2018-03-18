0d243b855c90b68409e8e81558a2f0496d389992
ggodreau <godreau@gmail.com>
2018-03-18 18:00:10 -0500

    64 :ideograph_advantage: working geolocation via console.log :syringe:

--- section6/src/app/app.module.ts
+++ section6/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { Camera } from '@ionic-native/camera';
+import { Geolocation } from '@ionic-native/geolocation';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -25,6 +26,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     Camera,
+    Geolocation,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
--- section6/src/pages/home/home.ts
+++ section6/src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, Platform } from 'ionic-angular';
 import { Camera, CameraOptions } from '@ionic-native/camera';
+import { Geolocation } from '@ionic-native/geolocation';
 //import { AppVersion } from 'ionic-native';
 
 @Component({
@@ -13,6 +14,7 @@ export class HomePage {
   constructor(
     public navCtrl: NavController,
     private camera: Camera,
+    private geolocation: Geolocation,
     platform: Platform) {
 
       const options: CameraOptions = {
@@ -36,7 +38,24 @@ export class HomePage {
           console.log('camera error');
          // Handle error
         });
-      });
+
+        // GEOLOCATION
+        this.geolocation.getCurrentPosition().then((resp) => {
+                console.log(resp.coords.latitude);
+                console.log(resp.coords.longitude);
+            }).catch((error) => {
+                console.log('Error getting location', error);
+        });
+        
+        let watch = this.geolocation.watchPosition();
+          watch.subscribe((data) => {
+                // data can be a set of coordinates, or an error (if an error occurred).
+                // data.coords.latitude
+                // data.coords.longitude
+               console.log('watch: ', data); 
+                });
+
+        });
   }
 
 }
